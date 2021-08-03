30dce4b80d03cb77eb4af7bbc4cf75db6a1b890b
Reimar <reimarmetzen@gmail.com>
2021-08-03 23:00:42 +0200

    Speak glucose value middleware dummy

---
+++ LibreDirectPlayground/Modules/SensorGlucoseSpeak.swift
@@ -0,0 +1,31 @@
+//
+//  SensorGlucoseRead.swift
+//  LibreDirectPlayground
+//
+//  Created by Reimar Metzen on 03.08.21.
+//
+
+import Foundation
+import Combine
+import UserNotifications
+
+func sensorGlucoseSpeakMiddelware(service: SensorGlucoseSpeakService) -> Middleware<AppState, AppAction> {
+    return { state, action in
+        switch action {
+        case .setSensorReading(readingUpdate: let readingUpdate):
+            service.speakGlucose(glucose: readingUpdate.lastGlucose.glucoseFiltered.asGlucose(unit: state.glucoseUnit))
+
+        default:
+            break
+
+        }
+
+        return Empty().eraseToAnyPublisher()
+    }
+}
+
+class SensorGlucoseSpeakService {
+    func speakGlucose(glucose: String) {
+
+    }
+}
