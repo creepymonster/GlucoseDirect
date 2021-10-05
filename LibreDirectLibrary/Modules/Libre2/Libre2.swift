7a635af30724a43345bdbfec6b7934bc631fb713
creepymonster <reimarmetzen@gmail.com>
2021-10-05 21:22:48 +0200

    remove ios 15 checks

--- LibreDirectLibrary/Modules/Libre2/Libre2.swift
+++ LibreDirectLibrary/Modules/Libre2/Libre2.swift
@@ -8,7 +8,7 @@
 import Foundation
 import Combine
 
-@available(iOS 15.0, *)
+@available(iOS 13.0, *)
 public func libre2Middelware() -> Middleware<AppState, AppAction> {
     return deviceMiddelware(service: Libre2Service())
 }
