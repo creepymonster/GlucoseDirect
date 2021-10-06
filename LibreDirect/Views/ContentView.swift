07dba8e079d9b67c96fe4f1ef25aabea31936444
creepymonster <reimarmetzen@gmail.com>
2021-10-06 21:01:47 +0200

    add loop addon like spacing

--- LibreDirect/Views/ContentView.swift
+++ LibreDirect/Views/ContentView.swift
@@ -18,16 +18,16 @@ struct ContentView: View {
                 }
 
                 ConnectionView(connectionState: store.state.connectionState, connectionError: store.state.connectionError, connectionErrorTimestamp: store.state.connectionErrorTimeStamp, missedReadings: store.state.missedReadings).padding([.horizontal])
-
                 AlarmSnoozeView().padding([.top, .horizontal])
+
                 GlucoseChartView(glucoseValues: store.state.glucoseValues, glucoseUnit: store.state.glucoseUnit, alarmLow: store.state.alarmLow, alarmHigh: store.state.alarmHigh, targetValue: 100).padding([.top, .horizontal]).frame(minHeight: 200)
                 LifetimeView(sensor: store.state.sensor).padding([.top, .horizontal])
                 DetailsView(sensor: store.state.sensor).padding([.top, .horizontal])
 
                 GlucoseSettingsView().padding([.top, .horizontal])
+                
                 AlarmSettingsView().padding([.top, .horizontal])
                 NightscoutSettingsView().padding([.top, .horizontal])
-
                 ActionsView().padding([.top, .horizontal])
             }
         }
