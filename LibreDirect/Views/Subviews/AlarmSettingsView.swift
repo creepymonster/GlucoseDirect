8110bb99ea6aa0234b6667b9b638bbcf3917b82a
Reimar <reimarmetzen@gmail.com>
2021-11-27 09:34:51 +0100

    clean translations

--- LibreDirect/Views/Subviews/AlarmSettingsView.swift
+++ LibreDirect/Views/Subviews/AlarmSettingsView.swift
@@ -13,15 +13,15 @@ struct AlarmSettingsView: View {
     var body: some View {
         Section(
             content: {
-                ToggleView(key: LocalizedString("Glucose low/high", comment: ""), value: store.state.glucoseAlarm) { value -> Void in
+                ToggleView(key: LocalizedString("Glucose alarm", comment: ""), value: store.state.glucoseAlarm) { value -> Void in
                     store.dispatch(.setGlucoseAlarm(enabled: value))
                 }
                 
-                ToggleView(key: LocalizedString("Lifetime ends", comment: ""), value: store.state.expiringAlarm) { value -> Void in
+                ToggleView(key: LocalizedString("Wearing time alarm", comment: ""), value: store.state.expiringAlarm) { value -> Void in
                     store.dispatch(.setExpiringAlarm(enabled: value))
                 }
                 
-                ToggleView(key: LocalizedString("Connection issues", comment: ""), value: store.state.connectionAlarm) { value -> Void in
+                ToggleView(key: LocalizedString("Connection alarm", comment: ""), value: store.state.connectionAlarm) { value -> Void in
                     store.dispatch(.setConnectionAlarm(enabled: value))
                 }
                 
