e88c1746789b88a6c20b794da7b0e6443fbce343
Robert <comps@diprobe.com>
2021-12-09 09:32:31 +0100

    version number in settings screen

--- LibreDirect/Views/SettingsView.swift
+++ LibreDirect/Views/SettingsView.swift
@@ -9,9 +9,11 @@ struct SettingsView: View {
     // MARK: Internal
 
     @EnvironmentObject var store: AppStore
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
 
     var body: some View {
         VStack {
+            Text("GlucoseDirect v\(appVersion!)").foregroundColor(Color.ui.blue).font(.caption)
             List {
                 SensorConnectorSettings()
                 GlucoseSettingsView()
