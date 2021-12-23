894f9a1b3abac19c89ed8a9b783da822c09ef05e
Reimar <reimarmetzen@gmail.com>
2021-12-23 13:54:23 +0100

    add readaloudsettingsview

--- LibreDirect/Views/SettingsView.swift
+++ LibreDirect/Views/SettingsView.swift
@@ -16,6 +16,7 @@ struct SettingsView: View {
                 AlarmSettingsView()
                 NightscoutSettingsView()
                 CalendarExportSettingsView()
+                ReadAloudSettingsView()
                 OtherSettingsView()
                 AboutView()
             }
