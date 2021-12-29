d6ba1f05e310c7c6e40029f268b6810422fbcfbf
Reimar <reimarmetzen@gmail.com>
2021-12-29 17:04:09 +0100

    add facebook group url

--- LibreDirect/AppConfig.swift
+++ LibreDirect/AppConfig.swift
@@ -12,6 +12,7 @@ enum AppConfig {
     static var githubUrl = "https://github.com/creepymonster/GlucoseDirectApp"
     static var faqUrl = "https://github.com/creepymonster/GlucoseDirectApp"
     static var crowdinUrl = "https://crwd.in/glucose-direct-app"
+    static var facebookUrl = "https://www.facebook.com/groups/4747621411996068/"
 
     static var projectName = "GlucoseDirect"
 
