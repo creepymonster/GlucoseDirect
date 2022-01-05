9fcb662b189a9781eb94067b62848b995d667547
Reimar <reimarmetzen@gmail.com>
2022-01-05 23:50:56 +0100

    make compileable - but not selectable

--- LibreDirect/Views/Subviews/AlarmSettingsView.swift
+++ LibreDirect/Views/Subviews/AlarmSettingsView.swift
@@ -14,15 +14,27 @@ struct AlarmSettingsView: View {
         Section(
             content: {
                 ToggleView(key: LocalizedString("Glucose alarm", comment: ""), value: store.state.glucoseAlarm) { value -> Void in
-                    store.dispatch(.setGlucoseAlarm(enabled: value))
+                    if value {
+                        store.dispatch(.setGlucoseAlarmSound(sound: .alarm))
+                    } else {
+                        store.dispatch(.setGlucoseAlarmSound(sound: .none))
+                    }
                 }
 
                 ToggleView(key: LocalizedString("Wearing time alarm", comment: ""), value: store.state.expiringAlarm) { value -> Void in
-                    store.dispatch(.setExpiringAlarm(enabled: value))
+                    if value {
+                        store.dispatch(.setExpiringAlarmSound(sound: .expiring))
+                    } else {
+                        store.dispatch(.setExpiringAlarmSound(sound: .none))
+                    }
                 }
 
                 ToggleView(key: LocalizedString("Connection alarm", comment: ""), value: store.state.connectionAlarm) { value -> Void in
-                    store.dispatch(.setConnectionAlarm(enabled: value))
+                    if value {
+                        store.dispatch(.setConnectionAlarmSound(sound: .alarm))
+                    } else {
+                        store.dispatch(.setConnectionAlarmSound(sound: .none))
+                    }
                 }
             },
             header: {
