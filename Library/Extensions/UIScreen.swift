c4656f78f7b460de9cb6efee6f8c48b4ce798ffc
Reimar <reimarmetzen@gmail.com>
2022-11-01 18:39:32 +0100

    fix uiscreen

--- Library/Extensions/UIScreen.swift
+++ Library/Extensions/UIScreen.swift
@@ -6,7 +6,15 @@
 import SwiftUI
 
 extension UIScreen {
-   static let screenWidth = UIScreen.main.bounds.size.width
-   static let screenHeight = UIScreen.main.bounds.size.height
-   static let screenSize = UIScreen.main.bounds.size
+    static var screenWidth: CGFloat {
+        UIScreen.main.bounds.size.width
+    }
+
+    static var screenHeight: CGFloat {
+        UIScreen.main.bounds.size.height
+    }
+
+    static var screenSize: CGSize {
+        UIScreen.main.bounds.size
+    }
 }
