df236acfc2847fd708ac73c9bfbc8ed277bd25d3
Reimar Metzen <reimarmetzen@gmail.com>
2022-11-12 16:34:45 +0100

    fix debug output

--- App/Views/Lists/SensorGlucoseList.swift
+++ App/Views/Lists/SensorGlucoseList.swift
@@ -24,7 +24,7 @@ struct SensorGlucoseList: View {
                             Spacer()
 
                             if DirectConfig.isDebug, sensorGlucose.timestamp < smoothThreshold, DirectConfig.smoothSensorGlucoseValues {
-                                Text("smooth \(sensorGlucose.glucoseValue)")
+                                Text(sensorGlucose.glucoseValue.asGlucose(glucoseUnit: store.state.glucoseUnit))
                             }
 
                             if let glucoseValue = sensorGlucose.smoothGlucoseValue?.toInteger(), sensorGlucose.timestamp < smoothThreshold, DirectConfig.smoothSensorGlucoseValues {
