00be85dca291b958671dde31d65322b7795c95df
Reimar Metzen <reimarmetzen@gmail.com>
2023-01-18 19:50:44 +0100

    add simple watch app

---
+++ WatchApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Watch Watch App
+//
+//  Created by Reimar Metzen on 18.01.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchApp/WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp.swift
+//  Watch Watch App
+//
+//  Created by Reimar Metzen on 18.01.23.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
