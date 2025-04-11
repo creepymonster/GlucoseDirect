4418cab41a0fc853e9de5c249f9b89e798e92561
Reimar Metzen <reimarmetzen@gmail.com>
2025-04-11 21:20:23 +0200

    fix build error

--- Library/Content/Glucose.swift
+++ Library/Content/Glucose.swift
@@ -12,7 +12,7 @@ protocol Glucose: Equatable {
     var timestamp: Date { get }
     var glucoseValue: Int { get }
 
-    static func == (lhs: any Glucose, rhs: any Glucose) -> Bool
+    static func == (lhs: Self, rhs: Self) -> Bool
 }
 
 extension Glucose {
@@ -22,7 +22,7 @@ extension Glucose {
         return minutes % ofMinutes == 0
     }
 
-    static func == (lhs: any Glucose, rhs: any Glucose) -> Bool {
+    static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.id == rhs.id
     }
 }
